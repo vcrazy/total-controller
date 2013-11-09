0b5b692dad29075314ce5601143e33d687e2369c
vcrazy@abv.bg <vcrazy@abv.bg>
2013-11-09 22:08:42 +0200

    real data; test 1

---
+++ get_data/script.js
@@ -0,0 +1,96 @@
+var all_data = {},
+	start = + new Date,
+	sequence = 0,
+	min = 100,
+	waiting = 100,
+	total_requests = 0,
+	received_requests = 2;
+
+var step1 = function(){
+	console.log('Started on:', new Date);
+
+	$.get('https://irdatabase.globalcache.com/v1/manufacturers', function(data){
+//		console.log(data);
+
+		var l = data.length;
+		total_requests += l;
+		for(var i = 0; i < l; i++){
+			all_data[data[i].Key] = {};
+
+			sequence++;
+			setTimeout(step2, get_time(), data[i].Key);
+		}
+	});
+};
+
+var step2 = function(manufacturer){
+	$.get('https://irdatabase.globalcache.com/v1/manufacturers/'+ manufacturer + '/devicetypes', function(data){
+		received_requests++;
+//		console.log(data);
+
+		var l = data.length;
+		total_requests += l;
+		for(var i = 0; i < l; i++){
+			all_data[manufacturer][data[i].Key] = {};
+
+			sequence++;
+			setTimeout(step3, get_time(), manufacturer, data[i].Key);
+		}
+	});
+};
+
+var step3 = function(manufacturer, device){
+	$.get('https://irdatabase.globalcache.com/v1/manufacturers/'+ manufacturer + '/devicetypes/' + device + '/codesets', function(data){
+		received_requests++;
+
+		// here we should get the codeset only
+		// and we don't need to get all codesets because their differences
+		// are in a part that we don't use
+
+		sequence++;
+		total_requests += 1;
+		setTimeout(step4, get_time(), manufacturer, device, data[0].Key);
+	});
+};
+
+var step4 = function(manufacturer, device, codeset){
+	$.get('https://irdatabase.globalcache.com/v1/manufacturers/'+ manufacturer + '/devicetypes/' + device + '/codesets/' + codeset, function(data){
+		received_requests++;
+//		console.log(data);
+
+		var l = data.length;
+		for(var i = 0; i < l; i++){
+			if(!all_data[manufacturer][device][data[i].KeyName]){
+				all_data[manufacturer][device][data[i].KeyName] = [];
+			}
+
+			var code = data[i].IRCode.split(',').map(function(e){
+					return e * 1;
+				}),
+				frequency = code[0];
+
+			code.splice(0, 3); // remove the frequency and two ones (1)
+
+			all_data[manufacturer][device][data[i].KeyName].push({
+				frequency: frequency,
+				code: code
+			});
+		}
+
+		if(total_requests == received_requests){
+			console.log('Ready on:', new Date);
+			all_data = JSON.stringify(all_data);
+			console.log(all_data);
+//			window.localStorage.all_data = all_data; // too much information
+			$('body').append('<audio autoplay="autoplay"><source src="http://ganev.bg/v/DavidGuetta-YoureNotAlone.mp3" type="audio/mp3" /></audio>');
+		}
+
+	});
+};
+
+function get_time(){
+	return Math.max(min, start + sequence * waiting - + new Date);
+}
+
+// start
+step1();
--- totalcontroller.js
+++ totalcontroller.js
@@ -1,10 +1,21 @@
 var express = require('express'),
 	app = express(),
+	fs = require('fs'),
+	data_file = './get_data/data.txt',
 	defaults = {
 		port: 9119
 	},
 	Request,
-	Response;
+	Response,
+	all_data = {};
+
+fs.readFile(data_file, 'utf8', function(err, data) {
+	if (err){
+		throw err;
+	}
+
+	all_data = JSON.parse(data);
+});
 
 // start the drums
 app.listen(defaults.port);
@@ -14,30 +25,21 @@ app.get('/*', function(req, res){
 	var url = req.url.split('/'),
 		data = [];
 
-	url.shift(); // remove the first /
+	// remove the first '/' and the empty strings
+	url.shift();
+	url = url.filter(function(e){
+		return e;
+	});
 
 	if(url.length === 0){
 		// get all manufacturers
-
+		data = Object.keys(all_data);
 	}else if(url.length === 1){
-		// get devices by manufacturer
-
+		// get devices (by manufacturer)
+		data = Object.keys(all_data[url[0]] || {});
 	}else if(url.length === 2){
-		// get codes by manufacturer & device
-
-		// temp test data as you may suppose
-		data = [{ // key1:
-			key: 'On, Power On/Off',
-			manufacturer: 'LG',
-			device: 'TV',
-			codes: [{ // codeset1:
-				frequency: 38343,
-				code: [343,172,21,22,21,22,21,65,21,22,21,22,21,22,21,22,21,22,21,65,21,65,21,22,21,65,21,65,21,65,21,65,21,65,21,22,21,22,21,65,21,22,21,22,21,22,21,65,21,65,21,65,21,65,21,22,21,65,21,65,21,65,21,22,21,22,21,4907]
-			}, { // codest2:
-				frequency: 38226,
-				code: [343,171,21,22,21,22,21,65,21,22,21,22,21,22,21,22,21,22,21,65,21,65,21,22,21,65,21,65,21,65,21,65,21,65,21,22,21,22,21,65,21,22,21,22,21,22,21,65,21,65,21,65,21,65,21,22,21,65,21,65,21,65,21,22,21,22,21,4892]
-			}]
-		}];
+		// get codes (by manufacturer & device)
+		data = all_data[url[0]] && all_data[url[0]][url[1]] || {};
 	}
 
 	res.send(JSON.stringify(data));
